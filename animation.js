3b056c967ac519706b38a2a9425985c06c3b7ce3
Addison Andrew Blanda <tuf18810@cis.temple.edu>
2017-09-11 13:44:49 -0400

    hearts! 2

--- animation.js
+++ animation.js
@@ -1,18 +1,17 @@
-var animateMe = function(targetElement, speed, leftPos, topPos, opacity){
+var animateMe = function(targetElement, speed, leftPos, opacity){
     
-    $(targetElement).css({top:topPos});
     $(targetElement).css({left:leftPos})
     $(targetElement).css({opacity:opacity})
     $(targetElement).animate(
         {
-        'top': -$(document).width() + 1500,
+        'top': -$(document).width() + 2000,
         'opacity': "0%"
         }, 
         { 
         duration: speed, 
         complete: function(){
             leftPos = Math.random() * $(document).width();
-            animateMe(this, speed, leftPos, topPos, 1);
+            animateMe(this, speed, leftPos, 1);
             }
         }
     );
@@ -20,17 +19,17 @@ var animateMe = function(targetElement, speed, leftPos, topPos, opacity){
 };
 $(function(){
 	animateMe($('#object1'), 5000, Math.random() * $(document).width()
-, 500, 1);
+, 1);
 	animateMe($('#object2'), 7000, Math.random() * $(document).width()
-, 500, 1);
+, 1);
 	animateMe($('#object3'), 4000, Math.random() * $(document).width()
-, 500, 1);
+, 1);
 	animateMe($('#object4'), 8000, Math.random() * $(document).width()
-, 500, 1);
+, 1);
 	animateMe($('#object5'), 6000, Math.random() * $(document).width()
-, 500, 1);
+, 1);
 	animateMe($('#object6'), 7000, Math.random() * $(document).width()
-, 500, 1);
+, 1);
 	animateMe($('#object7'), 6500, Math.random() * $(document).width()
-, 500, 1);
+, 1);
 });
